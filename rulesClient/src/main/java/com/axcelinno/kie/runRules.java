0799a6b51713a20b9d1997bea5b13c2b1e51330e
samuelbennett <samuelbennett@10.192.71.38>
2019-09-18 16:20:07 -0500

    updating github

--- rulesClient/src/main/java/com/axcelinno/kie/runRules.java
+++ rulesClient/src/main/java/com/axcelinno/kie/runRules.java
@@ -15,7 +15,7 @@ public class runRules {
 			KieSession kSession = kContainer.newKieSession("session-rules");
 			
 			Product product = new Product(1,  "laptop", 150.00, 100, true);
-			Customer customer  = new Customer(1,  1,   400.00,  0.0,  false,  null,  62284457, 0.0, true);
+			Customer customer = new Customer(1, 1, 400.00, 0.0, true, null, 62284457, 0.0, true);
 			Shop shop = new Shop(false,  null,   null);
 			
 			FactHandle factHandle;
@@ -26,7 +26,61 @@ public class runRules {
 			kSession.fireAllRules();
 			System.out.println("Employee rule check");
 			System.out.println(product.getName() + " has " + product.getQuantity() + " left");
-			System.out.println("passes");
+			System.out.println("Customer spent: " + customer.getBalance());
+			System.out.println("Employeed status:  " + customer.getJobStatus());
+			System.out.println("Product price after employee discount: " + product.getPrice());
+			
+		} catch (Throwable t) {
+			// TODO: handle exception
+			t.printStackTrace();
+		}
+		try {
+			KieServices kieServices = KieServices.Factory.get();
+			KieContainer kContainer = kieServices.getKieClasspathContainer();
+			
+			KieSession kSession = kContainer.newKieSession("session-rules");
+			
+			Product product = new Product(1,  "laptop", 150.00, 100, true);
+			Customer customer2 = new Customer(2, 4, 650.0, 0.0, false, null, 45, 0.0, true);
+			Shop shop = new Shop(true,  null,   null);
+			
+			FactHandle factHandle;
+			factHandle = kSession.insert(product);
+			factHandle = kSession.insert(customer2);
+			factHandle = kSession.insert(shop);
+			
+			kSession.fireAllRules();
+			System.out.println("Adding non-employeed customer");
+			System.out.println(product.getName() + " has " + product.getQuantity() + " left");
+			System.out.println("Customer spent: " + customer2.getBalance());
+			System.out.println("Employeed status: " + customer2.getJobStatus());
+			System.out.println("Product price after holiday discount: " + product.getPrice());
+			
+		} catch (Throwable t) {
+			// TODO: handle exception
+			t.printStackTrace();
+		}
+		try {
+			KieServices kieServices = KieServices.Factory.get();
+			KieContainer kContainer = kieServices.getKieClasspathContainer();
+			
+			KieSession kSession = kContainer.newKieSession("session-rules");
+			
+			Product product = new Product(3,  "tv", 4100.00, 100, true);
+			Customer customer2 = new Customer(2, 4, 4300.00, 0.0, true, null, 62284457, 0.0, true);
+			Shop shop = new Shop(false,  null,   null);
+			
+			FactHandle factHandle;
+			factHandle = kSession.insert(product);
+			factHandle = kSession.insert(customer2);
+			factHandle = kSession.insert(shop);
+			
+			kSession.fireAllRules();
+			System.out.println("Employee, but product id is for TV");
+			System.out.println(product.getName() + " has " + product.getQuantity() + " left");
+			System.out.println("Customer spent: " + customer2.getBalance());
+			System.out.println("Employeed status: " + customer2.getJobStatus());
+			System.out.println("Product price after employee, but not discount not allowed for tv: " + product.getPrice());
 			
 		} catch (Throwable t) {
 			// TODO: handle exception
