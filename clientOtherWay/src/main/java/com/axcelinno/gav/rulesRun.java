b08c84e1b4597c0ebeb7a7b056f778fa11c9008c
samuelbennett <samuelbennett@10.192.66.59>
2019-09-19 11:24:44 -0500

    seeing if this works

--- clientOtherWay/src/main/java/com/axcelinno/gav/rulesRun.java
+++ clientOtherWay/src/main/java/com/axcelinno/gav/rulesRun.java
@@ -1,9 +1,14 @@
 package com.axcelinno.gav;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.kie.api.KieServices;
 import org.kie.api.builder.ReleaseId;
 import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieRuntime;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.runtime.rule.FactHandle;
 import com.myspace.*;
 
@@ -17,17 +22,27 @@ public class rulesRun {
 	public  static void main(String[] args) {
 		String groupID = "com.myspace.store";
 		String artifactId = "Store-drools";
-		String version = "1.0.1-SNAPSHOT";
+		String version = "1.0.7-SNAPSHOT";
+		
+		Map<String, Object> params = new HashMap<String, Object>();
+		Product product = new Product(1,  "laptop", 150.00, 100, true);
+		Customer customer = new Customer(1, 1, 400.00, 0.0, true, null, 62284457, 0.0, true);
+		Shop shop = new Shop(false,  null,   null);
+		params.put("c", customer);
+		params.put("p",  product);
+		params.put("s",  shop);
 		
 		KieServices ks = KieServices.Factory.get();
-		
 		ReleaseId releaseId = ks.newReleaseId(groupID,  artifactId,  version);
-		
 		KieContainer kContainer = ks.newKieContainer(releaseId);
-		
 		KieSession kSession = kContainer.newKieSession();
-		
-		kSession.fireAllRules();
+		KieRuntime kRuntime = (KieRuntime) kSession;
+		ProcessInstance processInstance = kRuntime.startProcess("Store-drools.shopProcess", params);
+		//kSession.fireAllRules();
+		System.out.println(customer.getBalance());
+		System.out.println(customer.getJobStatus());
+		System.out.println(product.getPrice());
+		System.out.println(product.getQuantity());
 		
 		System.out.println("works");
 	}
@@ -47,7 +62,8 @@ public class rulesRun {
 		factHandle = kSession.insert(c);
 		factHandle = kSession.insert(s);
 		
-		kSession.fireAllRules();
+		int num = kSession.fireAllRules();
+		System.out.println(num);
 	}
 
 }
--- clientOtherWay/src/test/java/com/axcelinno/gav/tests.java
+++ clientOtherWay/src/test/java/com/axcelinno/gav/tests.java
@@ -19,6 +19,41 @@ public class tests {
 		run.runMyRules(customer,  product,  shop);
 		assertEquals(99,  product.getQuantity());
 		assertEquals(430.00,  customer.getBalance(), 1e-8);
+		assertTrue(customer.getJobStatus());
+		assertEquals(105.0,  product.getPrice(), 1e-8);
+	}
+	
+	@Test
+	public void holiday() {
+		Product product = new Product(1,  "laptop", 150.00, 100, true);
+		Customer customer = new Customer(1, 1, 400.00, 0.0, false, null, 16, 0.0, true);
+		Shop shop = new Shop(true,  null,   null);
+		rulesRun run = new rulesRun();
+		run.runMyRules(customer,  product,  shop);
+		assertTrue(shop.getIsHoliday());
+		assertEquals(135.0,  product.getPrice(), 1e-8);
+	}
+	
+	@Test
+	public void TV() {
+		Product product = new Product(3,  "tv", 4100.00, 100, true);
+		Customer customer = new Customer(1, 1, 400.00, 0.0, true, null, 16, 0.0, true);
+		Shop shop = new Shop(true,  null,   null);
+		rulesRun run = new rulesRun();
+		run.runMyRules(customer,  product,  shop);
+		assertEquals(4100.00,  product.getPrice(), 1e-8);
+		assertTrue(customer.getJobStatus());
+	}
+	
+	@Test
+	public void noCheckout() {
+		Product product = new Product(1,  "laptop", 150.00, 100, true);
+		Customer customer = new Customer(1, 1, 400.00, 0.0, true, null, 62284457, 0.0, false);
+		Shop shop = new Shop(false,  null,   null);
+		rulesRun run = new rulesRun();
+		run.runMyRules(customer,  product,  shop);
+		assertEquals(295,  customer.getBalance(), 1e-8);
+		assertEquals(105.0, customer.getDiscountAmt(), 1e-8);
 	}
 
 }
